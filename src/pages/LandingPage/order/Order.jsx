99a3f989f89a11f43f1d760692c92e135acf8677
rickyraihan <rickyraihan83@gmail.com>
2023-12-16 14:21:48 +0700

    update

--- src/pages/LandingPage/order/Order.jsx
+++ src/pages/LandingPage/order/Order.jsx
@@ -1,6 +1,7 @@
 import Layout from "@/components/Layout";
 import Cart from "@/components/order/Cart";
 import CompanyProfile from "@/components/order/CompanyProfile";
+import { lazy, startTransition, useState } from "react";
 
 function Order() {
   const [currentStep, setCurrentStep] = useState(0);
